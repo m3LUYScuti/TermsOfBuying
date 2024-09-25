13f810376f9afd30e75224d20b9dfdf9a4dcc07f
davit <davit@GOSH>
2024-09-25 17:06:22 +0400

    Add project files.

---
+++ TermsOfBuying/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.WriteLine("USA? Yes or NO :");
+string BuyerCountry = Console.ReadLine();
+
+Console.WriteLine("Asaki:");
+int BuyerAge = int.Parse(Console.ReadLine());
+
+if (BuyerCountry == "Yes" && BuyerAge <= 20)
+{
+
+    Console.WriteLine("No");
+}
+
+else if (BuyerCountry == "Yes" && BuyerAge >= 21)
+
+{
+    Console.WriteLine("Yes");
+}
+
+else if (BuyerCountry == "No" && BuyerAge <= 17)
+
+{
+    Console.WriteLine("No");
+}
+
+else if (BuyerCountry == "No" && BuyerAge >= 18)
+
+{
+    Console.WriteLine("Yes");
+}
